12d4d75d51e901a9da02caae5062c9817d53ce27
Victor Aranguren <victoraranguren.dev@gmail.com>
2020-09-30 12:49:21 -0400

    crearTarea() Listo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 //Variables
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
+const listGroup = document.querySelector('.list-group');
 
 //Funciones
-const revision=(e)=>{
+const revision = (e) => {
     e.preventDefault();
+    const tarea = input.value;
     let respuesta;
-    if(input.value != ""){
-       respuesta = true;
-    }else {
+    if (tarea != undefined) {
+        respuesta = true;
+        crearTarea(tarea);
+    } else {
         respuesta = false;
     }
     console.log(respuesta);
 }
 
+const crearTarea = (tarea) => {
+    const el = document.createElement('li');
+    el.className = "list-group-item";
+    el.innerText = tarea;
+    listGroup.appendChild(el);
+}
+
 //Listeners
-form.addEventListener('submit',revision);
+form.addEventListener('submit', revision);
