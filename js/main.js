1862ca4ea699a4396f2ce852f41841b97c0f248d
Victor Aranguren <victoraranguren.dev@gmail.com>
2020-09-30 11:58:42 -0400

    Estructuta basica de la logica

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Hola");
+//Variables
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+
+//Funciones
+const revision=(e)=>{
+    e.preventDefault();
+    let respuesta;
+    if(input.value != ""){
+       respuesta = true;
+    }else {
+        respuesta = false;
+    }
+    console.log(respuesta);
+}
+
+//Listeners
+form.addEventListener('submit',revision);
