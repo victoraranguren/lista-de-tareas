5aae5f387b88ccc5af2ca87b2d39333cd12b9ed5
Victor Aranguren <victoraranguren.dev@gmail.com>
2020-09-30 16:34:55 -0400

    Logica mejoraao

--- js/main.js
+++ js/main.js
@@ -3,32 +3,10 @@ const form = document.querySelector('#form');
 const input = document.querySelector('#input');
 const listGroup = document.querySelector('.list-group');
 
-//Modelo
-const tareas = [
-    {
-        text: "Elemento 1"
-    },
-    {
-        text: "Elemento 2"
-    },
-    {
-        text: "Elemento 3"
-    },
-]
-
-tareas.forEach(tarea=>{
-    let el = document.createElement('li');
-    el.innerText = tarea.text;
-    el.className = 'list-group-item';
-    listGroup.appendChild(el)
-    console.log(el);
-})
-
-
 //Funciones
 const revision = (e) => {
     e.preventDefault();
-    const tarea = input.value;
+    let tarea = input.value;
     let respuesta;
     if (tarea != undefined) {
         respuesta = true;
@@ -44,7 +22,35 @@ const crearTarea = (tarea) => {
     el.className = "list-group-item";
     el.innerText = tarea;
     listGroup.appendChild(el);
+    setLS(tarea);
 }
 
+const setLS = (tarea) => {
+    if (localStorage.getItem('tareas') == undefined) {
+        let tareas = [];
+        tareas.push(tarea);
+        localStorage.setItem('tareas',JSON.stringify(tareas))
+    }else {
+        let tareas = JSON.parse(localStorage.getItem('tareas'));   
+        tareas.push(tarea);
+        localStorage.setItem('tareas',JSON.stringify(tareas))
+    }
+}
+
+const getLS=() => {
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    if (tareas == undefined) {
+        true
+    } else {
+    tareas.forEach(tarea => {
+        let el = document.createElement('li');
+        el.innerText = tarea;
+        el.className = 'list-group-item';
+        listGroup.appendChild(el)
+        console.log(el);
+    })
+    }
+}
 //Listeners
 form.addEventListener('submit', revision);
+document.addEventListener('DOMcontentloaded',getLS())
