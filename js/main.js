f88d1351c55e9dd9f5659e853d473b690712b159
Victor Aranguren <victoraranguren.dev@gmail.com>
2020-10-03 12:51:52 -0400

    Borarr curso de LS

--- js/main.js
+++ js/main.js
@@ -17,8 +17,12 @@ const revision = (e) => {
 
 const crearTarea = (tarea) => {
     const el = document.createElement('li');
+    const x = document.createElement('span');
     el.className = "list-group-item";
     el.innerText = tarea;
+    x.innerText = "X";
+    x.className = 'boton-borrar';
+    el.appendChild(x);
     listGroup.appendChild(el);
     setLS(tarea);
 }
@@ -42,13 +46,31 @@ const getLS = () => {
     } else {
         tareas.forEach(tarea => {
             let el = document.createElement('li');
+            const x = document.createElement('span');
             el.innerText = tarea;
             el.className = 'list-group-item';
-            listGroup.appendChild(el)
+            x.innerText = "X";
+            x.className = 'boton-borrar';
+            el.appendChild(x);
+            listGroup.appendChild(el);
             console.log(el);
         })
     }
 }
+const borrarTarea = (e) => {
+    //Del DOM
+    let tarea = e.target.parentElement.innerText;
+    let tareas = JSON.parse(localStorage.getItem('tareas'));
+    let tareaBorrar = tarea.substring(0, tarea.length - 1);
+
+    tareas.forEach((tarea, index) =>{
+        if(tareaBorrar == tarea) {
+             tareas.splice(index, 1);
+        }
+   }) ;
+
+};
 //Listeners
 form.addEventListener('submit', revision);
 document.addEventListener('DOMcontentloaded', getLS())
+document.querySelector('.boton-borrar').addEventListener('click', borrarTarea)
